0d354900f59ba94665f06c45ce8ba8421a9ecbfa
Manish Choudhary <mc5134455@gmail.com>
2024-07-10 18:58:11 +0530

    hi

---
+++ Javascript/html.js
@@ -0,0 +1,17 @@
+var a = 10
+let b = 'mxnish'
+let d = "choudhary"
+const c = false
+
+let firstName = 'apple'
+let lastName = 'iPhone 14 Pro max'
+
+console.log(a)
+// console.log(b)
+console.log(c)
+// console.log(d)
+console.log(`${b} ${d}`) // this method is used for spacing.
+console.log(`${firstName} ${lastName}`)
+console.log('mxnish\nchoudhary') // \n used for line break
+
+
